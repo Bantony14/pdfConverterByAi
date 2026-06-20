f81afd560f0d7277cedec844bf5f0395a45dd075
bantony <bantonysin95@gmail.com>
2026-06-20 23:21:54 +0530

    fix: update cors for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,8 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://bantony-pdf-converter-by-ai.vercel.app",
+        credentials: true
     })
 );
 
